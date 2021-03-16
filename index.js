14ac3581d9017856af1048445ae547f3a5091d72
acm-sp85 <acontell@gmail.com>
2021-03-16 10:09:03 -0400

    commit tree 2a52863ae38bbdb62f6fc25dadb250055292be86

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 31 , num2 = 2, num3=20, num4=84;
 
-// Write your code here
+let multiply = num1*num2;
+
+let random = Math.floor(Math.random()*10)+1;
+
+let mod = num4 % num3;
+
+let max = Math.max(num2,num3,4,5);
